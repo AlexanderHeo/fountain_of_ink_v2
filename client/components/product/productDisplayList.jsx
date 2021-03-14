61c217b7256a70669b227b1efa7558a69ff29730
Alexander Heo <jikster712@gmail.com>
2021-03-14 16:41:39 -0700

    product display list - setup to display rows of 3

--- client/components/product/productDisplayList.jsx
+++ client/components/product/productDisplayList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Picture from '../../../server/public/images/samplepic.jpg'
 
-class ProductDisplay extends Component {
+class ProductDisplayList extends Component {
 	state = {
 	  mounted: false
 	}
@@ -23,12 +23,32 @@ class ProductDisplay extends Component {
 	          <div className='product-image-button-text'>{this.props.match.params.category}</div>
 	        </div>
 	      </div>
+	      <div className='product-display-list'>
+	        <div className='product'>
+	          <img className='product-image' src={`/${Picture}`} />
+	          <div className='product-name'>{this.props.match.params.category}</div>
+	          <div className='product-description'>Ipsem Lorem</div>
+	          <div className='product-price'>$80</div>
+	        </div>
+	        <div className='product'>
+	          <img className='product-image' src={`/${Picture}`} />
+	          <div className='product-name'>{this.props.match.params.category}</div>
+	          <div className='product-description'>Go Go Power Rangers</div>
+	          <div className='product-price'>$400</div>
+	        </div>
+	        <div className='product'>
+	          <img className='product-image' src={`/${Picture}`} />
+	          <div className='product-name'>{this.props.match.params.category}</div>
+	          <div className='product-description'>This is the way</div>
+	          <div className='product-price'>$66</div>
+	        </div>
+	      </div>
 	    </Container>
 	  )
 	}
 }
 
-export default ProductDisplay
+export default ProductDisplayList
 
 const Container = styled.div`
 	width: 100%;
@@ -48,9 +68,22 @@ const Container = styled.div`
 		align-items: center;
 		flex-flow: column;
 		background-color: rgba(255, 255, 255, 0.7);
+		border-radius: 0.3rem;
 		padding: 3rem 0;
 	}
 	.product-image-button-text {
 		font-size: 2.3rem;
+		padding: 1rem 3rem;
+	}
+
+	.product-display-list {
+		display: flex;
+		justify-content: center;
+		margin: 10rem 0;
+	}
+	.product {
+		width: 30%;
+		text-align: center;
+		margin: 0 calc((100vw * .1) / 6);
 	}
 `
