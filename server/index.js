a1c66cf187672d68dd8fd2ff31ec3d94bdc1f4b5
Alexander Heo <jikster712@gmail.com>
2021-03-14 21:34:04 -0700

    new endpoint /api/fountainpens/:parameter - for filtering

--- server/index.js
+++ server/index.js
@@ -27,7 +27,8 @@ app.get('/api/pens', (req, res, next) => {
 		"s"."name" as "sale",
 		"p"."price",
 		"p"."colors",
-		"p"."penId" as "id"
+		"p"."penId" as "id",
+		"p"."type"
 		FROM "pens" as "p"
 		LEFT JOIN "brand" as "b" using ("brandId")
 		LEFT JOIN "category" as "c" using ("categoryId")
@@ -40,6 +41,59 @@ app.get('/api/pens', (req, res, next) => {
       res.json(result.rows)
     })
     .catch(err => next(err))
+})
+
+app.get('/api/fountainpens/:parameter', (req, res, next) => {
+  const { parameter } = req.params
+  if (!parameter) return next(new ClientError('Product parameter missing', 400))
+  let sql
+  if (parameter === 'all') {
+    sql = `
+		SELECT "p"."name",
+		"b"."name" as "brand",
+		"c"."name" as "category",
+		"s"."name" as "sale",
+		"p"."price",
+		"p"."colors",
+		"p"."penId" as "id",
+		"p"."type"
+		FROM "pens" as "p"
+		LEFT JOIN "brand" as "b" using ("brandId")
+		LEFT JOIN "category" as "c" using ("categoryId")
+		LEFT JOIN "sales" as "s" using ("salesId")
+		ORDER BY "penId" DESC;
+	`
+    db.query(sql)
+      .then(result => {
+        if (!result) return next(new ClientError('Internal error - code333', 400))
+        res.json(result.rows)
+      })
+      .catch(err => next(err))
+  } else {
+    sql = `
+		SELECT "p"."penId" as "id",
+			"p"."name",
+			"b"."name" as "brand",
+			"p"."colors",
+			"c"."name" as "category",
+			"s"."name" as "sales",
+			"p"."price",
+			"p"."type"
+			FROM "pens" as "p"
+			LEFT JOIN "brand" as "b" using ("brandId")
+			LEFT JOIN "category" as "c" using ("categoryId")
+			LEFT JOIN "sales" as "s" using ("salesId")
+			WHERE "type" = $1
+			ORDER BY "id"
+		`
+    const param = [parameter]
+    db.query(sql, param)
+      .then(result => {
+        if (!result) return next(new ClientError('Internal error code-334', 400))
+        res.json(result.rows)
+      })
+      .catch(err => next(err))
+  }
 })
 
 app.use('/api', (req, res, next) => {
